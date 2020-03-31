21dc75533360d36a8ad2021ddc59a8a06f96851e
Rozzini <Rozzinimr@gmail.com>
2020-04-01 03:14:54 +0800

    Added some comments

--- Snake/Food.cs
+++ Snake/Food.cs
@@ -6,12 +6,6 @@ namespace Snake
 {
     class Food : Point
     {
-        //Random random = new Random();
-        //public Food(int x, int y)
-        //{
-        //    this.x = random.Next(1, x);
-        //    this.y = random.Next(1, y);
-        //    this.ch = '0';
-        //}
+        
     }
 }
